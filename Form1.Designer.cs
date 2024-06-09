8351b4efc5b26692d3b34a811488b2f6ead43bad
DenzelDharielAP <dhariel0209@gmail.com>
2024-06-08 21:16:04 -0400

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace Prueba
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtHijos = new System.Windows.Forms.TextBox();
+            this.txtSueldoBruto = new System.Windows.Forms.TextBox();
+            this.txtIncentivoHijos = new System.Windows.Forms.TextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtSFS = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtAFP = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtSueldoNeto = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtDescuentos = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtHijos
+            // 
+            this.txtHijos.Location = new System.Drawing.Point(199, 137);
+            this.txtHijos.Name = "txtHijos";
+            this.txtHijos.Size = new System.Drawing.Size(100, 20);
+            this.txtHijos.TabIndex = 0;
+            this.txtHijos.Text = "0";
+            // 
+            // txtSueldoBruto
+            // 
+            this.txtSueldoBruto.Location = new System.Drawing.Point(199, 68);
+            this.txtSueldoBruto.Name = "txtSueldoBruto";
+            this.txtSueldoBruto.Size = new System.Drawing.Size(100, 20);
+            this.txtSueldoBruto.TabIndex = 1;
+            // 
+            // txtIncentivoHijos
+            // 
+            this.txtIncentivoHijos.Location = new System.Drawing.Point(199, 202);
+            this.txtIncentivoHijos.Name = "txtIncentivoHijos";
+            this.txtIncentivoHijos.Size = new System.Drawing.Size(100, 20);
+            this.txtIncentivoHijos.TabIndex = 2;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(403, 251);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(131, 37);
+            this.btnCalcular.TabIndex = 3;
+            this.btnCalcular.Text = "calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(196, 121);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Cantidad de hijos:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(196, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Sueldo bruto:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(196, 186);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(96, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Incentivo por hijos:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(196, 252);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(85, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Descuento SFS:";
+            // 
+            // txtSFS
+            // 
+            this.txtSFS.Location = new System.Drawing.Point(199, 268);
+            this.txtSFS.Name = "txtSFS";
+            this.txtSFS.Size = new System.Drawing.Size(100, 20);
+            this.txtSFS.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(196, 310);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(85, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Descuento AFP:";
+            // 
+            // txtAFP
+            // 
+            this.txtAFP.Location = new System.Drawing.Point(199, 326);
+            this.txtAFP.Name = "txtAFP";
+            this.txtAFP.Size = new System.Drawing.Size(100, 20);
+            this.txtAFP.TabIndex = 9;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(196, 428);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(67, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Sueldo neto:";
+            // 
+            // txtSueldoNeto
+            // 
+            this.txtSueldoNeto.Location = new System.Drawing.Point(199, 444);
+            this.txtSueldoNeto.Name = "txtSueldoNeto";
+            this.txtSueldoNeto.Size = new System.Drawing.Size(100, 20);
+            this.txtSueldoNeto.TabIndex = 11;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(196, 371);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(92, 13);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Total descuentos:";
+            // 
+            // txtDescuentos
+            // 
+            this.txtDescuentos.Location = new System.Drawing.Point(199, 387);
+            this.txtDescuentos.Name = "txtDescuentos";
+            this.txtDescuentos.Size = new System.Drawing.Size(100, 20);
+            this.txtDescuentos.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(879, 577);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtDescuentos);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtSueldoNeto);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtAFP);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtSFS);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.txtIncentivoHijos);
+            this.Controls.Add(this.txtSueldoBruto);
+            this.Controls.Add(this.txtHijos);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtHijos;
+        private System.Windows.Forms.TextBox txtSueldoBruto;
+        private System.Windows.Forms.TextBox txtIncentivoHijos;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtSFS;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtAFP;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtSueldoNeto;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtDescuentos;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Prueba
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            int Hijos = 0;
+            decimal IncentivoHijos = 0;
+            decimal SFS;
+            decimal AFP;
+            decimal Descuentos;
+            decimal SueldoBruto;
+            decimal SueldoNeto;
+
+
+            SueldoBruto = Convert.ToDecimal(txtSueldoBruto.Text);
+            Hijos = Convert.ToInt32(txtHijos.Text);
+
+            if (Hijos > 0)
+            {
+                IncentivoHijos = Hijos * 500;
+            }
+            else
+            {
+                MessageBox.Show("No aplica para incentivo de hijos","Aviso",MessageBoxButtons.OK,MessageBoxIcon.Information);
+            }
+
+            SFS = (SueldoBruto * 3.04m) / 100;
+            AFP = (SueldoBruto * 2.87m) / 100;
+            Descuentos = SFS + AFP;
+
+
+            SueldoNeto = (SueldoBruto + IncentivoHijos) - Descuentos;
+
+            txtIncentivoHijos.Text = IncentivoHijos.ToString();
+            txtSFS.Text = SFS.ToString();
+            txtAFP.Text = AFP.ToString();
+            txtDescuentos.Text = Descuentos.ToString();
+            txtSueldoNeto.Text = SueldoNeto.ToString();
+        }
+    }
+}
